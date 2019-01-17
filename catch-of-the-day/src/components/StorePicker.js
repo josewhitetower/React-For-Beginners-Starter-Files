3843851bde0c3e93b4e95c3a74ee228a85261ef0
Jose Torreblanca <josewhitetower@gmail.com>
2019-01-17 20:51:34 +0100

    added routes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  gotToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    console.log();
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form onSubmit={this.gotToStore} className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
         <button type="submit">Visit Store ➞ </button>
       </form>
     );
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import StorePicker from "./components/StorePicker";
+import Router from "./components/Router";
+
 import App from "./components/App";
 import "./css/style.css";
 
-render(<App />, document.querySelector("#main"));
+render(<Router />, document.querySelector("#main"));
